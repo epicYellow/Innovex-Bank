990c99a164c78040a2036964495f0402de62a2b7
epicYellow <105212574+epicYellow@users.noreply.github.com>
2023-08-08 00:13:24 +0200

    Bindings set up for Header and Topsection
    Header Controlls Bindings
    TopsectionView bindings

--- ContentViews/PageHeader.xaml.cs
+++ ContentViews/PageHeader.xaml.cs
@@ -2,8 +2,17 @@ namespace Innovex_Bank.ContentViews;
 
 public partial class PageHeader : ContentView
 {
+	public static BindableProperty PageProperty =
+		BindableProperty.Create(nameof(PageTitle), typeof(string), typeof(PageHeader), default(string));
+
+	public string PageTitle
+	{
+		get => (string)GetValue(PageProperty);
+		set => SetValue(PageProperty, value);
+	}
 	public PageHeader()
 	{
 		InitializeComponent();
+		BindingContext = this;
 	}
 }
