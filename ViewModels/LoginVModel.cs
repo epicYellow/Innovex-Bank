998db19f5572025291b52f7b0b2307bea670b101
justin koster <200100@virtualwindow.co.za>
2023-08-09 12:20:40 +0200

    added some shadows and login binding
    made a basic binding for the login where it observes the entry text and adds it to a list, will be reworked when backend is incorporated

---
+++ ViewModels/LoginVModel.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Innovex_Bank.ViewModels;
+
+public partial class LoginVModel : ObservableObject
+{
+    public MainLoginModel()
+    {
+        Items = new ObservableCollection<string>();
+    }
+
+    [ObservableProperty]
+    ObservableCollection<string> items;
+
+    [ObservableProperty]
+    string text;
+
+    [RelayCommand]
+    void Add() => Text = string.Empty;
+}
+
