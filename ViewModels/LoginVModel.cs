737c04afd40f7488e854c2943b613415922a1113
justin koster <200100@virtualwindow.co.za>
2023-08-09 18:12:40 +0200

    start login bindnings
    only a starting point but the bindings need to be reworked completely

--- ViewModels/LoginVModel.cs
+++ ViewModels/LoginVModel.cs
@@ -11,18 +11,30 @@ namespace Innovex_Bank.ViewModels;
 
 public partial class LoginVModel : ObservableObject
 {
-    public MainLoginModel()
+    public LoginVModel()
     {
+        //items are users
         Items = new ObservableCollection<string>();
     }
 
+
+    //Items can be login details
     [ObservableProperty]
     ObservableCollection<string> items;
 
+    //text is input feild
     [ObservableProperty]
     string text;
 
+
+    //adds text to items, can match with backend to verify user.
     [RelayCommand]
-    void Add() => Text = string.Empty;
+    void Add()
+    {
+        if (string.IsNullOrWhiteSpace(Text))
+            return;
+        Items.Add(Text);
+        Text = string.Empty;
+    }
 }
 
