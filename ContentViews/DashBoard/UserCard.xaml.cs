726abdbae61c40353e13ae8c615f196d1dfa9cdb
justin koster <200100@virtualwindow.co.za>
2023-08-13 01:36:36 +0200

    Bindings for UserOverView Cards
    User Overview card bindings for username and role.

--- ContentViews/DashBoard/UserCard.xaml.cs
+++ ContentViews/DashBoard/UserCard.xaml.cs
@@ -2,8 +2,30 @@ namespace Innovex_Bank.ContentViews.DashBoard;
 
 public partial class UserCard : ContentView
 {
-	public UserCard()
+
+    public static BindableProperty UsernameProperty =
+       BindableProperty.Create(nameof(Username), typeof(string), typeof(UserCard), default(string));
+
+    public static BindableProperty RoleProperty =
+       BindableProperty.Create(nameof(Role), typeof(string), typeof(UserCard), default(string));
+
+    public string Username
+    {
+        get => (string)GetValue(UsernameProperty);
+        set => SetValue(UsernameProperty, value);
+    }
+
+    public string Role
+    {
+        get => (string)GetValue(RoleProperty);
+        set => SetValue(RoleProperty, value);
+    }
+
+    public UserCard()
 	{
 		InitializeComponent();
-	}
+        BindingContext = this;
+    }
 }
+
+
